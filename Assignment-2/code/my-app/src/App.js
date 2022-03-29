0af5422ed7df8ae117fdfac1b9345d9d639871d5
Deepak Malik <deepak22196@gmail.com>
2022-03-29 15:52:55 +0530

    react-assignment-2 submission

--- Assignment-2/code/my-app/src/App.js
+++ Assignment-2/code/my-app/src/App.js
@@ -1,18 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-
-
-function App(props) {
- const {employee}=props;
-    
-return(
-  <div className="container">
-  {/* Start your React code here */}
-</div>
-)
+function App({ employee }) {
+  return (
+    <div className="container">
+      <img className="image" src={employee.profileImg} alt="employee-image" />
+      <div className="info">
+        <br />
+        <h1 style={{ color: "dodgerblue" }}>{employee.name}</h1>
+        <br />
+        <h3 style={{ color: "darkgrey" }}>location</h3>
+        <h2>{employee.location}</h2>
+        <br />
+        <h3 style={{ color: "darkgrey" }}>Blood Group</h3>
+        <h2>{employee.bloodGroup}</h2>
+        <br />
+        <h3 style={{ color: "darkgrey" }}>Age</h3>
+        <h2>{employee.age}</h2>
+      </div>
+    </div>
+  );
 }
-
-
 
 export default App;
